03298b20df40940ae057b5662a5eb252c7bb55a4
max3a3 <maxmax3a3@gmail.com>
2024-03-07 18:25:56 -0800

    add doc

--- canvas/vite.config.ts
+++ canvas/vite.config.ts
@@ -1,5 +1,5 @@
 import { lezer } from "@lezer/generator/rollup"
-import react from "@vitejs/plugin-react"
+import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 import glsl from "vite-plugin-glsl"
 import { ViteRsw } from "vite-plugin-rsw"
@@ -8,4 +8,9 @@ import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react(), awaits(), ViteRsw(), glsl(), lezer()],
+
+  server:{
+    port:8808,
+    hmr:false//true//false // auto/manmual refresh
+  },
 })
